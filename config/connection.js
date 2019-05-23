f57893a007b04599daf743040856f535741a4e93
Ewunet Kassa <ewunetk@yahoo.com>
2019-05-23 16:50:43 -0400

    w/o password

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Etba@084113",
+  password: "",
   database: "burgers_db"
 });
 
